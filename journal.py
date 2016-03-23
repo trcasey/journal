dfbf60cf64d63125d4a05bb285fc65a291d17503
Trent Casey <trcasey@outlook.com>
2016-03-22 20:09:37 -0400

    Added docstrings to journal.py.

--- journal.py
+++ journal.py
@@ -1,6 +1,16 @@
+"""
+This is the journal module.
+"""
+
 import os
 
 def load(name):
+    """
+    This method creates and loads a new journal.
+
+    :param name: This is base name of the journal to load.
+    :return: A new journal data structure populated with the file data
+    """
     data = []
     filename = get_full_pathname(name)
 
@@ -13,6 +23,13 @@ def load(name):
 
 
 def save(name, journal_data):
+    """
+    This method saves a new journal entry.
+
+    :param name: This is the base name of the journal to load.
+    :param journal_data: This is the actual content of the journal entry.
+    :return:
+    """
     filename = get_full_pathname(name)
     print("...saving to: {}".format(filename))
 
@@ -23,8 +40,21 @@ def save(name, journal_data):
 
 
 def get_full_pathname(name):
+    """
+    This method gets the full pathname for the journal file.
+
+    :param name: This is the base name of the journal to load.
+    :return: The full path to the journal file.
+    """
     filename = os.path.abspath(os.path.join('.', 'journals', name + '.jrl'))
     return filename
 
 def add_entry(text, journal_data):
+    """
+    This method adds a new entry to the journal file.
+
+    :param text: This is the base name of the journal to load.
+    :param journal_data: This is the actual content of the journal entry to add to the file.
+    :return:
+    """
     journal_data.append(text)
